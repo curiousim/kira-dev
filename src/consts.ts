fe150abef288d28fabee544037a97cdc0877dcbe
Kira Menshov <kira.mnshv@gmail.com>
2025-11-15 15:32:09 +0200

    Front page responsivness done

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const INTERNAL_LINKS = [
+	{ href: "/", title: "Home" },
+	{ href: "/about", title: "About" },
+	{ href: "/blog", title: "Blog" },
+];
