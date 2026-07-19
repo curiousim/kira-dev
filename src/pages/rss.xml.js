c2ec575915e0c12ad26abc9c28597c5d46154363
Kira Menshov <kira.mnshv@gmail.com>
2026-07-19 07:48:27 +0300

    fix: repair RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,15 +1,19 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection("posts"))
+		.filter((post) => !post.data.draft)
+		.sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime());
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.pubDate,
 			link: `/blog/${post.id}/`,
 		})),
 	});
