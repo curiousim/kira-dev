233bd06405d1ad44ff93cbbacda67d8e7e429464
Kira Menshov <kira.mnshv@gmail.com>
2026-07-19 07:48:15 +0300

    fix: update site metadata constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Kira Menshov";
+export const SITE_DESCRIPTION =
+	"Personal site and blog of Kira Menshov, front-end engineer. Notes on web development and whatever else I'm learning.";
 
 export const INTERNAL_LINKS = [
 	{ href: "/", title: "Home" },
